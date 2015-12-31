7a874130a6d462440476e09c7a5ab5fb80f9de5d
helpfulrobot <chris@silverstripe.com>
2016-01-01 00:13:13 +1300

    Converted to PSR-2

--- code/WidgetPage.php
+++ code/WidgetPage.php
@@ -3,48 +3,48 @@
 /**
 * Widget Page
 */
-class WidgetPage extends Page {
+class WidgetPage extends Page
+{
 
-	private static $has_many = array(
-		'Widgets' => 'Widget.WidgetPage'
-	);
+    private static $has_many = array(
+        'Widgets' => 'Widget.WidgetPage'
+    );
 
-	function getCMSFields(){
-		$fields = parent::getCMSFields();
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
 
-		$adder = new GridFieldAddNewMultiClass();
+        $adder = new GridFieldAddNewMultiClass();
 
-		if(is_array($this->config()->get("allowed_widgets"))){
-			$adder->setClasses($this->config()->get("allowed_widgets"));
-		}
+        if (is_array($this->config()->get("allowed_widgets"))) {
+            $adder->setClasses($this->config()->get("allowed_widgets"));
+        }
 
-		$config = GridFieldConfig_RecordEditor::create()
-			->removeComponentsByType("GridFieldAddNewButton")
-			->addComponent($adder)
-			->addComponent(new GridFieldOrderableRows());
+        $config = GridFieldConfig_RecordEditor::create()
+            ->removeComponentsByType("GridFieldAddNewButton")
+            ->addComponent($adder)
+            ->addComponent(new GridFieldOrderableRows());
 
-		$fields->addFieldToTab("Root.Main",
-			GridField::create('Widgets','Widgets',$this->Widgets())
-				->setConfig($config)
-			,"Content"
-		);
-		$fields->removeByName("Content");
-		return $fields;
-	}
-
+        $fields->addFieldToTab("Root.Main",
+            GridField::create('Widgets', 'Widgets', $this->Widgets())
+                ->setConfig($config), "Content"
+        );
+        $fields->removeByName("Content");
+        return $fields;
+    }
 }
 
 /**
 * WidgetPage_Controller
 */
-class WidgetPage_Controller extends Page_Controller{
-
+class WidgetPage_Controller extends Page_Controller
+{
 }
 
-class WidgetPageWidget extends DataExtension{
+class WidgetPageWidget extends DataExtension
+{
 
-	static $has_one = array(
-		'WidgetPage' =>  'WidgetPage'
-	);
-
+    public static $has_one = array(
+        'WidgetPage' =>  'WidgetPage'
+    );
 }
